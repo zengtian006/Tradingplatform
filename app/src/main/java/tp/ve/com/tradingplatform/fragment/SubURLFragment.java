2c7f712f0bc61cb4304d0613d65bb11feb232080
Zeng <zengtian006@gmail.com>
2015-11-18 12:35:50 +0800

    tim

--- app/src/main/java/tp/ve/com/tradingplatform/fragment/SubURLFragment.java
+++ app/src/main/java/tp/ve/com/tradingplatform/fragment/SubURLFragment.java
@@ -1,5 +1,12 @@
 package tp.ve.com.tradingplatform.fragment;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.inputmethodservice.Keyboard;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,26 +14,39 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 import tp.ve.com.tradingplatform.R;
+import tp.ve.com.tradingplatform.activity.MainActivity;
 import tp.ve.com.tradingplatform.activity.ShareActivity;
+
+import android.view.inputmethod.InputMethodManager;
 
 /**
  * Created by Zeng on 2015/11/17.
  */
 public class SubURLFragment extends Fragment {
+    private static final String TAG = SubURLFragment.class.getSimpleName();
 
     EditText edt_title, edt_url;
+    ImageView view_image;
+    String img;
+    Bitmap bitmap;
+    ProgressDialog pDialog;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,10 +57,12 @@ public class SubURLFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_sub_url, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_sub_url, container, false);
         edt_url = (EditText) rootView.findViewById(R.id.urlText);
         edt_url.setText(ShareActivity.urlString);
         edt_title = (EditText) rootView.findViewById(R.id.text_title);
+        view_image = (ImageView) rootView.findViewById(R.id.view_image);
+
         Button btn_fetch = (Button) rootView.findViewById(R.id.btn_fetch);
         btn_fetch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +73,10 @@ public class SubURLFragment extends Fragment {
                     if (!url.contains("http://")) {
                         url = "http://" + url;
                     }
+                    InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                    inputManager.hideSoftInputFromWindow(rootView.getWindowToken(),
+                            InputMethodManager.HIDE_NOT_ALWAYS);
                     new DownloadTask().execute(url);
                 }
 
@@ -69,11 +95,10 @@ public class SubURLFragment extends Fragment {
             Document doc = Jsoup.connect(urlString).get();
             title = doc.title();
             Elements imgs = doc.getElementsByTag("img");
-            Log.v("test", title);
-//            for (Element el : imgs) {
-//                img = el.attr("abs:src");
-//                ims.add(img);
-//            }
+            for (Element el : imgs) {
+                img = el.attr("abs:src");
+                Log.v(TAG, img);
+            }
         } finally {
             if (stream != null) {
                 stream.close();
@@ -84,6 +109,15 @@ public class SubURLFragment extends Fragment {
     }
 
     private class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(getActivity());
+            pDialog.setMessage("Fetching URL ....");
+            pDialog.show();
+        }
+
         @Override
         protected String doInBackground(String... urls) {
             try {
@@ -99,15 +133,40 @@ public class SubURLFragment extends Fragment {
             super.onPostExecute(result);
             Log.i("URLFragment", result);
             if (result.equals("Wrong")) {
+                pDialog.dismiss();
                 Toast.makeText(getActivity(), "Can not fetch this URL", Toast.LENGTH_SHORT).show();
                 edt_url.requestFocus();
                 edt_url.selectAll();
             } else {
                 edt_title.setText(result);
-
+                new LoadImage().execute(img);
             }
+        }
+    }
 
+    private class LoadImage extends AsyncTask<String, String, Bitmap> {
+
+        protected Bitmap doInBackground(String... args) {
+            try {
+                bitmap = BitmapFactory.decodeStream((InputStream) new URL(args[0]).getContent());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
         }
 
+        protected void onPostExecute(Bitmap image) {
+
+            if (image != null) {
+                view_image.setImageBitmap(image);
+                pDialog.dismiss();
+            } else {
+                pDialog.dismiss();
+                Toast.makeText(getActivity(), "Image Does Not exist or Network Error", Toast.LENGTH_SHORT).show();
+
+            }
+        }
     }
+
 }
